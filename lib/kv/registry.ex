c89eaa51ee85c6eca8659d1c3094010feaef4f5d
Rabea Gleissner <rabeagleissner@gmail.com>
2019-07-03 17:54:28 +0800

    Add client api functions for registry

--- lib/kv/registry.ex
+++ lib/kv/registry.ex
@@ -1,19 +1,38 @@
 defmodule KV.Registry do
   use GenServer
 
+
+  #Client API functions
+
+  def start_link(opts) do
+    #second argument is passed into init/1 callback
+    GenServer.start_link(__MODULE__, :ok, opts)
+  end
+
+  def lookup(server, name) do
+    #second argument (tuple) must match first argument of handle_call
+    GenServer.call(server, {:lookup, name})
+  end
+
+  def create(server, name) do
+    GenServer.cast(server, {:create, name})
+  end
+
+  #Server functions
+
   @impl true
   def init(:ok) do
     {:ok, %{}}
   end
 
   @impl true
-  # calls are synchronous
+  #calls are synchronous
   def handle_call({:lookup, name}, _from, names) do
     {:reply, Map.fetch(names, name), names}
   end
 
   @impl true
-  # casts are asynchronous
+  #casts are asynchronous
   def handle_cast({:create, name}, names) do
     if Map.has_key?(names, name) do
       {:noreply, names}
